f897f223b470cb904b0081c18c3ef83b7058af85
ritesh singh <rootritesh64@gmail.com>
2020-08-03 08:18:26 +0530

    th

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -2,18 +2,38 @@ import 'package:flutter/material.dart';
 
 myApp() {
   var cont = Container(
-      height: 320,
+      alignment: Alignment.center,
+      height: double.infinity,
       width: double.infinity,
       color: Colors.black45,
-      margin: EdgeInsets.all(50),
-      padding: EdgeInsets.all(50),
-      child: Text('This is text inside container')
-      // decoration: BoxDecoration(
-      //   color: Colors.red.shade400,
-      //   borderRadius: BorderRadius.circular(20),
-      //   border: Border.all(width: 2),
-      // )
-      );
+      margin: EdgeInsets.all(20),
+      // padding: EdgeInsets.all(50),
+      child: Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.all(30),
+            // margin: EdgeInsets.all(10),
+            // padding: EdgeInsets.all(50),
+            alignment: Alignment.center,
+            width: 300,
+            height: 300,
+            color: Colors.red,
+            child: Text('hei'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              // color: Colors.red.shade400,
+              borderRadius: BorderRadius.circular(20),
+              border: Border.all(width: 2),
+            ),
+            alignment: Alignment.center,
+            width: 100,
+            height: 100,
+            // color: Colors.blue,
+          ),
+        ],
+      ));
 
   var myhome = Scaffold(
     appBar: AppBar(
